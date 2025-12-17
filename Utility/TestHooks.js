f406bcfe71be8a0a69757449c45fb26ac4e035ef
mfaizan <muhammad.faizan@bedm.pk>
2025-12-17 20:19:43 +0500

    Hooks are being updated as for CI

--- Utility/TestHooks.js
+++ Utility/TestHooks.js
@@ -1,21 +1,25 @@
 const { Before, After, AfterAll } = require('@cucumber/cucumber')
-const page = require('@playwright/test')
+const { chromium } = require('playwright');
 const { AllureRuntime } = require('allure-cucumberjs');
 
 Before(async () => {
-  let browser = await page.chromium.launch({ headless: false })
+  let browser = await page.chromium.launch({ headless: true })
   global.browser = browser
-  const context = await browser.newContext()
-  global.page = await context.newPage()
+
+  global.context = await browser.newContext();
+  global.page = await context.newPage();
+
+  this.context = global.context;
+  this.page = global.page;
 })
 After(async function (scenario) {
   if (scenario.result.status === Status.FAILED) {
       const screenshot = await this.page.screenshot({ path: `allure-results/${scenario.pickle.name}.png` });
       this.attach(screenshot, 'image/png');
   }
-  await this.page.close();
-  await this.context.close();
+  if(this.page) await this.page.close();
+  if(this.context) await this.context.close();
 });
 AfterAll(async () => {
-  await global.browser.close()
+  if(global.browser)await global.browser.close();
 })
